d5ebfde6bed588350abd66c05efd1871b36d26dc
dgnsrekt <run2devtest@gmail.com>
2018-10-07 13:13:45 -0500

    init

---
+++ gunbot.py/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+BASEPATH = Path(__file__).parent
+
+GUNBOT_DOWNLOAD_PATH = BASEPATH.parent / "lin.zip"
+
+GUNBOT_DOWNLOAD_URL = "https://github.com/GuntharDeNiro/BTCT/releases/download/v1072836/lin.zip"
