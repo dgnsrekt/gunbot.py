d4c9d5fd59b381d61cbea7016d92d9b3d9ffd256
dgnsrekt <run2devtest@gmail.com>
2018-10-13 13:10:53 -0500

    updated gunbot url

--- gunbot_python/paths.py
+++ gunbot_python/paths.py
@@ -8,4 +8,8 @@ BASEPATH = Path(__file__).parent
 
 GUNBOT_DOWNLOAD_PATH = BASEPATH.parent / "lin.zip"
 
-GUNBOT_DOWNLOAD_URL = "https://github.com/GuntharDeNiro/BTCT/releases/download/v1072836/lin.zip"
+GUNBOT_DOWNLOAD_URL = "https://github.com/GuntharDeNiro/BTCT/releases/download/v1072b837/lin.zip"
+
+GUNBOT_PATH = BASEPATH.parent / "gunbot"
+
+TEMP_GUNBOT_EXTRACTION_PATH = BASEPATH.parent / "gunbot_temp"
